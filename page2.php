5bc49c3272928df2ddf567ac0a4e6ee3c6ef5cb4
Катя <98916224+katrina4323@users.noreply.github.com>
2022-02-02 23:46:05 +0300

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mysite2</title>
+    <style>
+    	body {
+    		background-image: url(img/fon2.jpg);
+    		margin-top: 150px;
+    	}
+    	h1 {
+    		text-align: center;
+    	}
+    </style>
+</head>
+<body>
+	<h1>Благодарю вас за внимание!</h1>
+</body>
+</html>
